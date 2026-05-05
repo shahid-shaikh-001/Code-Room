ecf0b76fa9ea7c5388d3f88fab686604927fdd86
Shahid Shaikh <shahidsocials.007@gmail.com>
2026-05-05 18:04:17 +0530

    trying to fixing the error for sevalla deployment app

--- Backend/src/lib/inngest.js
+++ Backend/src/lib/inngest.js
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js";
-export const inngest = new Inngest({ id: "my-app" });
+export const inngest = new Inngest({ id: "Code-Room" });
 
 const syncUser = inngest.createFunction(
   {
