6067ef1197efd24a79fd2eb42ffdc2eba3ae30d0
Shahid Shaikh <shahidsocials.007@gmail.com>
2026-05-06 15:32:44 +0530

    adding on variable for inngest.js

--- Backend/src/lib/inngest.js
+++ Backend/src/lib/inngest.js
@@ -1,7 +1,7 @@
 import { Inngest } from "inngest";
 import { connectDB } from "./db.js";
 import User from "../models/User.js"; 
-import { upsertStreamUser } from "./stream.js";
+import { upsertStreamUser , deleteStreamUser} from "./stream.js";
 
 export const inngest = new Inngest({ id: "my-app" });
 
