b5e9016b084aee8b31a30e0952b3ffc26fb0309c
Shahid Shaikh <shahidsocials.007@gmail.com>
2026-05-07 02:20:50 +0530

    fixing typo error in the files

--- Backend/src/controller/chatController.js
+++ Backend/src/controller/chatController.js
@@ -9,7 +9,7 @@ export async function getStreamToken(req,res) {
             token,
             userId: req.user.clerkId,
             userName:req.user.name,
-            userImage:req.user.image
+            userImage:req.user.Image
         })
     } catch (error) {
         console.log("Error in getStreamToken controller:", error.message);
